4763201c25edcc71f45e3e684e34587f37bc8e25
Epsilon3271 <149324840+Epsilon3271@users.noreply.github.com>
2025-02-04 16:05:35 +0100

    Update main.js

--- Projet_WEB/JavaScript/main.js
+++ Projet_WEB/JavaScript/main.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     let score = 0;
+    let bute = 10;
     const scoreElement = document.getElementById("score");
     const button = document.getElementById("click-button");
 
     button.addEventListener("click", function() {
         score++;
+        if (score == bute) {
+            alert(`Score de ${bute} atteint`);
+            bute *= 2;
+        }
         scoreElement.textContent = score;
     });
 });
